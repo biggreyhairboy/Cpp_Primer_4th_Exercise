8b8a957e802ce171829093e88d8fff6a6c0056bf
biggreyhairboy@gmail.com <YANGJK46yjk19890412>
2015-05-19 21:49:17 +0800

    chapter 01 is done

---
+++ Chapter01/Chapter01.cpp
@@ -0,0 +1,164 @@
+//1.1 not tested on MAC VM Windows
+
+//1.2 not tested on MAC VM Windows
+
+//1.3
+//#include <iostream>
+//
+//void main(){
+//	std::cout << "hello world!" << std::endl;
+//	getchar();
+//}
+
+//1.4
+//#include <iostream>
+//using namespace std;
+//
+//void main(){
+//	int a = 3, b = 4;
+//	int c = a + b;
+//	int d = a * b;
+//	cout << "c = " << c << endl;
+//	cout << "d = " << d << endl;
+//	getchar();
+//}
+
+//1.5 pass
+//1.6 pass
+
+//1.7 pass
+//1.8 pass
+//1.9 pass
+
+//1.10 1.11
+//#include <iostream>
+//using namespace std;
+//
+//void main(){
+//	int sum_for = 0;
+//	for (int i = 50; i <= 100; i++){
+//		sum_for += i;
+//	}
+//	cout << "sum_for = " << sum_for << endl;
+//	int sum_while = 0;
+//	int n = 50;
+//	while (n <= 100){
+//		sum_while += n;
+//		n++;
+//	}
+//	cout << "sum_while = " << sum_while << endl;
+//	getchar();
+//}
+
+//1.12pass
+//1.13pass
+
+//1.14 1.15 pass
+
+//1.16
+//#include <iostream>
+//
+//void main(){
+//	std::cout << "Enter two numbers" << std::endl;
+//	int a, b;
+//	std::cin >> a >> b;
+//	if (a >= b){
+//		std::cout << a;
+//	}
+//	else {
+//		std::cout << b;
+//	}
+//	getchar();
+//}
+
+//1.17
+//#include <iostream>
+//using namespace std;
+//
+//void main(){
+//	int a[10];
+//	for (int n = 0; n < 10; n++){
+//		cin >> a[n];
+//	}
+//	int counter = 0;
+//	for (int i = 0; i < 10; i++){
+//		if (a[i] < 0){
+//			counter++;
+//		}
+//	}
+//	cout << counter << " negative numbers in the array!" << endl;
+//	int b = 0;
+//	cin >> b;
+//	getchar();
+//}
+
+//1.18
+//#include <iostream>
+//using namespace std;
+//void main(){
+//	std::cout << "Enter two numbers" << std::endl;
+//	int a, b;
+//	std::cin >> a >> b;
+//	int begin, end;
+//	if (a >= b){
+//		begin = b;
+//		end = a;
+//	}
+//	else {
+//		begin = a;
+//		end = b;
+//	}
+//	if (a == b || (end - begin) == 1){
+//		cout << "no number between" << endl;
+//	}
+//	for (int i = begin + 1; i < end; i++){
+//		cout << i << " ";
+//	}
+//	cin >> a;
+//	getchar();
+//}
+
+//1.19
+//#include <iostream>
+//#include <iomanip>
+//using namespace std;
+//void main(){
+//	std::cout << "Enter two numbers" << std::endl;
+//	int a, b;
+//	std::cin >> a >> b;
+//	int begin, end;
+//	if (a >= b){
+//		begin = b;
+//		end = a;
+//	}
+//	else {
+//		begin = a;
+//		end = b;
+//	}
+//	if (a == b || (end - begin) == 1){
+//		cout << "no number between" << endl;
+//	}
+//	for (int i = begin + 1; i < end; i++){
+//		
+//	 	cout<<setw(12)  << i << " ";
+//		if (((i - begin)  % 5) == 0){
+//			cout << endl;
+//		}
+//	}
+//	cin >> a;
+//	getchar();
+//}
+
+//1.20 pass
+
+//todo
+//1.21 1.22 1.23 1.24 1.25 1.26
+
+//#include <iostream>
+//using namespace std;
+//
+//void main(){
+//
+//	getchar();
+//}
+
